19444a1a2fee74791c0a045cbcb78f54030cc86d
thanh dang <dangthanh.fe@gmail.com>
2023-04-03 11:11:45 +0700

    home work Js3

---
+++ main.js
@@ -0,0 +1,24 @@
+var numbers = document.getElementsByClassName('btn');
+var input = document.getElementById('input');
+var result = document.getElementById('result');
+for (number of numbers) { 
+    number.addEventListener('click', function () {
+        input.innerHTML += this.value;
+    });
+}
+
+function handleAC() { 
+    input.innerHTML = '';
+    result.innerHTML = '';
+}
+
+function equal() { 
+    var res = input.innerHTML;
+    var output = eval(res);
+    result.innerHTML = output;
+}
+
+function del() { 
+    var res = input.innerHTML;
+    input.innerHTML = res.substring(0, res.length - 1)
+}
